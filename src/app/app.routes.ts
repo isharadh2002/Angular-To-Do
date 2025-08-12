fdcd96e3cfc45151a413201d0573d2d30b3bbabc
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-08-12 16:20:03 +0530

    Angular App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list';
+import { AddTask } from './components/add-task/add-task';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/active', component: TaskListComponent, data: { filter: 'active' } },
+  { path: 'tasks/completed', component: TaskListComponent, data: { filter: 'completed' } },
+  { path: 'add', component: AddTask },
+
+];
